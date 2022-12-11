327a7193cb0eff68d86ee77d078e14960e5b10ef
DylanGermann <dev.dyger@gmail.com>
2022-12-11 18:32:31 +0100

    add contact

--- src/Containers/Contact/Contact.js
+++ src/Containers/Contact/Contact.js
@@ -2,7 +2,15 @@ import React from 'react'
 
 export default function Contact() {
   return (
-    <>
-    </>
+    <div className="container-contact">
+      <h1>Contactez-nous</h1>
+      <p>Par mail: react@code@.fr</p>
+      <p>Par téléphone: +33 6 12 12 34 54</p>
+      <ul>
+        <li>Facebook</li>
+        <li>Twitter</li>
+        <li>Youtube</li>
+      </ul>
+    </div>
   )
 }
